a90d4ba4d48cf95049866522a136c8c5b84931eb
James <james@ilmlaser.com>
2022-12-20 12:59:48 -0500

    starting with a simple idea

---
+++ config.py
@@ -0,0 +1,13 @@
+# This should be the general config for the tradebot
+# change the client to match client.txt path
+# 
+# 
+
+
+resolution = 800x600
+
+trade_msg = "Hi, I would like to buy your"
+
+give_me_msg = "I'm gunna give you the goods"
+
+client = "C:/Program Files (x86)/Grinding Gear Games/Path of Exile/logs/Client.txt"
